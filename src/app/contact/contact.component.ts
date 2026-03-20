3dde33f8280bcb1d492a3a00af935cf077e16a40
arikhativa <arikhativa@gmail.com>
2026-03-20 10:18:14 +0200

    changed gmap url

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -27,7 +27,7 @@ export class ContactComponent implements OnInit {
 
     // Raw URL for the map
     private readonly rawMapUrl =
-        'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3345.961917540149!2d35.0863773!3d32.9123497!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x151db06f9762141d%3A0xc3f5a25055b88234!2z15TXp9eo16ksINeo16vXmSDXp9eV!5e0!3m2!1siw!2sil!4v1700000000000'
+        'https://www.google.com/maps?q=32.8992071,35.0870633&z=17&hl=en'
 
     // This will hold the sanitized version
     public safeMapUrl!: SafeResourceUrl
