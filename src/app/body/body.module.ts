d778c9d2fda0352fcece311c6661f90d46804de1
arikhativa <arikhativa@gmail.com>
2026-04-20 12:07:27 +0300

    added image opt

--- src/app/body/body.module.ts
+++ src/app/body/body.module.ts
@@ -2,7 +2,7 @@ import { NavbarComponent } from './../navbar/navbar.component'
 import { NgModule } from '@angular/core'
 import { IntroComponent } from '../intro/intro.component'
 import { RouterModule } from '@angular/router'
-import { CommonModule } from '@angular/common'
+import { CommonModule, NgOptimizedImage } from '@angular/common'
 import { LogoComponent } from '../logo/logo.component'
 import { VidComponent } from '../vid/vid.component'
 import { GalleryComponent } from '../gallery/gallery.component'
@@ -14,7 +14,6 @@ import { MultiImage4Component } from '../multi-image-4/multi-image-4.component'
 import { MultiImage2Component } from '../multi-image-2/multi-image-2.component'
 import { MultiImage3Component } from '../multi-image-3/multi-image-3.component'
 import { SingleImageComponent } from '../single-image/single-image.component'
-import { AboutSectionComponent } from '../about-section/about-section.component'
 import { ProductsComponent } from '../products/products.component'
 import { ProductsHeroComponent } from '../products-hero/products-hero.component'
 import { ProductsStoreComponent } from '../products-store/products-store.component'
@@ -43,7 +42,6 @@ import { ContactComponent } from '../contact/contact.component'
         MultiImage2Component,
         MultiImage3Component,
         SingleImageComponent,
-        AboutSectionComponent,
         ProductsComponent,
         ProductsHeroComponent,
         ProductsStoreComponent,
@@ -63,6 +61,7 @@ import { ContactComponent } from '../contact/contact.component'
         CardModule,
         ButtonModule,
         RouterModule,
+        NgOptimizedImage,
         CommonModule,
         GalleriaModule,
     ],
