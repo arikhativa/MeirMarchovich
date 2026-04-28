7ca2c390aa97281aa79483cbd18385390d66593a
arikhativa <arikhativa@gmail.com>
2026-04-28 08:39:07 +0300

    chore: rename Marchovich to Marcovich

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    css: ['./dist/MeirMarchovich/browser/*.css'],
+    css: ['./dist/MeirMarcovich/browser/*.css'],
     content: [
-        './dist/MeirMarchovich/browser/index.html',
-        './dist/MeirMarchovich/browser/*.js',
+        './dist/MeirMarcovich/browser/index.html',
+        './dist/MeirMarcovich/browser/*.js',
     ],
     safelist: [/^grid/, /^col-/, /^sm:/, /^md:/, /^lg:/, /^xl:/],
-    output: './dist/MeirMarchovich/browser/',
+    output: './dist/MeirMarcovich/browser/',
 }
